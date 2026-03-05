4c438ec991622de9e2cfb3c618342b229a91ce0f
Hoezl <happyhome9854@gmail.com>
2026-03-05 16:44:17 +0900

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -13,7 +13,7 @@ import streamlit.components.v1 as components
 
 # ==========================================
 # 🔑 제미나이 API 키 설정
-GEMINI_API_KEY = "AIzaSyCr1DPYLBiLK-SjkFdCj8bQsfkMYdPlceU"
+GEMINI_API_KEY = "AIzaSyASvCj6a-dLCWB8ldbt5mqVEu1FtqUOEEs"
 genai.configure(api_key=GEMINI_API_KEY)
 # ==========================================
 
@@ -697,3 +697,4 @@ elif menu == "📚 영어 기초 가이드":
         의문문이 다른 문장 속으로 쏙 들어갈 때. 진짜 질문이 아니므로 어순이 평서문으로 바뀜.
         * 간접: I don't know **who he is**. (의문사+주어+동사)
         """)
+
